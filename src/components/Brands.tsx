1975a755e1ff70031a58d48e7a549937a241a980
youngseolg-beep <youngseolg@gmail.com>
2026-05-18 11:24:25 +0900

    Update Brands.tsx

--- src/components/Brands.tsx
+++ src/components/Brands.tsx
@@ -11,7 +11,7 @@ export default function Brands({ lang }: { lang: "ko" | "en" }) {
   const brands = [
     {
       name: lang === "ko" ? "본가" : "BORNGA",
-      logo: "https://i.ibb.co/6q7JGsB/image.png",
+      logo: "https://i.ibb.co/HT50xv8z/image.png",
       imageUrl: "https://i.ibb.co/jkrTJ0xK/2.jpg",
       videoUrl: "https://www.youtube.com/embed/L4JR9SuM8hQ",
       details:
